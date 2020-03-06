7d02800125719924df41aedc5f69a6c690921578
niekas <albertasgim@gmail.com>
2020-03-06 07:09:27 +0200

    Add assignment2 checking functions

--- tasks/assignments/__init__.py
+++ tasks/assignments/__init__.py
@@ -55,3 +55,28 @@ def show_assignment2(username=None):
 
     except Exception as exc:
         print(f'ERROR: could not retrieve Assignment Nr 2, because {exc}')
+
+
+def check_assignment2(username=None):
+    if not username:
+        username = getpass.getuser()
+    try:
+        import requests
+        import json
+        import base64
+        import dill
+
+        objects = {e: getattr(__main__, e) for e in dir(__main__)
+                if not e.startswith('_') and
+                e not in ['In', 'Out', 'exit', 'get_ipython', 'quit', 'check_assignment1', 'show_assignment1']}
+        pickled_objects = base64.b64encode(dill.dumps(objects))
+
+        resp = requests.post(f'https://python.lt/api/oop/2/check/{username}/', data={'objects': pickled_objects})
+
+        if resp.ok:
+            print(resp.text)
+        else:
+            print(f'ERROR: could not check Assignment Nr 2, because got {resp.status_code}, reason {resp.text}')
+
+    except Exception as exc:
+        print(f'ERROR: could not check Assignment Nr 2, because {exc}')
